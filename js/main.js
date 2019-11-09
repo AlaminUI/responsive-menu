db28ef9ede28176fa4d177d9a00ba6c7f777a600
AlaminAB <alaminislam907@gmail.com>
2019-11-09 15:06:57 +0600

    scroll update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 
   var displayMenu= document.getElementById("displayMenu");
-  var x = document.getElementById("mobile-menu");
+  var mobileMenu = document.getElementById("mobile-menu");
    displayMenu.addEventListener("click",function(){
-    x.classList.toggle("menu-block");
+    mobileMenu.classList.toggle("menu-block");
     console.log("working")
    })
 
+// humberg menu icon
 var menu=document.querySelector(".menu-icon");
 var icon1=document.querySelector(".first-icon");
 var icon2=document.querySelector(".secound-icon");
 var icon3=document.querySelector(".third-icon");
-
 menu.addEventListener("click",function(){
-   icon2.classList.toggle("none");
-  icon1.classList.toggle("icon");
-   icon3.classList.toggle("icons");
+   icon2.classList.toggle("toggle-none");
+  icon1.classList.toggle("toggle-icon");
+   icon3.classList.toggle("toggle-icons");
 })
+
+
+// scroll top border change
+
+  var navBar = document.querySelector("#navBar");
+ window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > navBar.offsetHeight || document.documentElement.scrollTop > navBar.offsetHeight) {
+    navBar.classList.add("navbar-full-border");
+    mobileMenu.classList.remove("mobile-border");
+  } else {
+      navBar.classList.remove("navbar-full-border");
+        mobileMenu.classList.add("mobile-border");
+  }
+}
